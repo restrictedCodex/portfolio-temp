4484faa9d9057effafe5459d44c5d76917176f48
restrictedCodex <65292146+restrictedCodex@users.noreply.github.com>
2021-04-19 20:21:58 +0530

    updated resume

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,7 +2,7 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: 'portfolio', // e.g: 'Name | Developer'
   lang: 'en', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
